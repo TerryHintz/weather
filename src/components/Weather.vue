bd3f47276c11811b16ada242c4fbee9b8c00241a
TerryHintz <cza82@sfu.ca>
2020-09-09 11:42:08 -0700

    init

---
+++ src/components/Weather.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="root">
+    <div class="weather-root">
+      <input type="text" class="input-box">
+    </div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Weather extends Vue {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.root {
+  min-height: 100vh;
+  background-image: url('https://images.unsplash.com/photo-1530908295418-a12e326966ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80');
+}
+.weather-root {
+  max-width: 500px;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+}
+.input-box {
+  font-size: 25px;
+}
+</style>
